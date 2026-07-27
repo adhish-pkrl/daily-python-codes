9442a42d9b81289abf588d6fe404f1d6cb35a7e3
adhish-pkrl <adhishpokharel105@gmail.com>
2026-07-27 21:49:46 +0545

    First Python commit

---
+++ day1.py
@@ -0,0 +1,87 @@
+"""what is python 
+python is a high level language, Developed BY  Guido van Rossum (1991)"""
+
+#W3 school best for learning /
+#python.org x
+
+#bhind the scen C / that's because its fast (at backend of python)
+"""
+Web deve
+AI 
+machine learning 
+mathmatics
+System Scripting
+
+Why python?
+Windows, MAc , linux, Resberry Pi
+
+syntax : pain text into english
+focus on Logic 
+easy to write 
+no need of Compiler 
+OOB programming language hoo
+
+"""
+
+#print("Hello World")
+#indentation refers to the space 
+
+#here is our first program to Print Statement 
+#print("Hello Guys Im Adhish POkharel")
+
+#X = 3
+#print("the value of x is ", X)
+#print('Hello') # single quotation  panii  same kam garxa kam garxa 
+
+#print(3)
+
+#Now Variables
+
+#data lai Sotre garnii containers 
+
+#A variable is created at first assign a Value 
+
+#statement 
+#numeric
+
+
+#a = 5
+#b = "Adhish"
+#print(a, b)
+#print(f"value of {a} my name is{b}")
+
+#print(type(a))
+#print(type(b))  ctrl + slash /
+
+# x = 10
+# y = 5
+# print(f"the value of {x} and the value of {y}")
+# print(type(x))
+
+
+#rules of making variable
+"""
+cannot start with number 
+9x = 4 // error
+case sensitive 
+a variable must be start with letter and Underscore 
+can contain only alpha numeric character // b4 = 5 --RUn // 4b = 3 - shows error
+cannot use @, $ % & in variables 
+pre-Defined keywords cannot be Used while declaring the variable // if = 4 - Shows error 
+cannot give space  " he llo = 5  "
+
+"""
+
+first_name1 = "Adhish"
+last_name22 = "Pokharel"
+print("first name is : ", first_name1)
+
+
+# CASTING 
+x = str(3)
+y= "pokharel"
+z = int(3.5)
+
+
+voli Multi variables;;
+aani global variable
