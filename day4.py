98bc45d2062268aa7cac0aaa4509accf92ae93e0
adhish-pkrl <adhishpokharel105@gmail.com>
2026-07-30 21:44:25 +0545

    Day 4 ho haii Boolen haru yeta uta

---
+++ day4.py
@@ -0,0 +1,27 @@
+#Boolean 
+#represents True or false
+
+print(6>7)   #expression evaluatd
+
+#Statement 
+a = 8
+b = 4
+if a>b: 
+    print("a is greater then b")
+else:
+
+    print("b is greater")
+
+
+#most values are true herniii ekchoti 
+print(bool())  #empty string false aauxa
+print(bool("hi")) #true hunxaa
+
+print(bool(0))    #false hunxa 0 maa
+print(bool(11))   #true aauxa
+
+#touples khallii vaye false 
+print(bool([])) #false aauxa 
+print(bool(["apple"]))  #true
+
+
