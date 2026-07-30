42c5ab2b24bd9a133fbaa9ab631a48a8ccaed006
adhish-pkrl <adhishpokharel105@gmail.com>
2026-07-30 23:01:25 +0545

    operator Self learning

---
+++ self_Dy4.py
@@ -0,0 +1,52 @@
+##Operatorss 
+#Arithmetic vaiiSakyo
+
+
+"""
+
+"""
+
+#Assignment Operators
+
+x = 5  #" = " ley x ma 5 ko value dinxa
+print(x)
+
+x += 4
+print(x)
+
+x -= 2
+print(x)
+
+x *= 3
+print(x)
+
+x/=2
+print(x)
+
+x %= 3
+print(x)
+
+x // 3
+print(x)
+
+x **= 2
+print(x)
+
+#x &= 3
+#print(x)
+
+#x |= 3 
+#print(x)
+
+#x ^= 3
+#print(x)
+
+#x >>= 3
+# print(x)
+
+# x <<= 3
+# print(x) 
+
+
+
+
