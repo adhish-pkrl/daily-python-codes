b83ca7b8ae3c520690c0742870cd7c1ab478e331
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-03 20:07:47 +0545

    Day 5 -->lists

---
+++ pythnday5.py
@@ -0,0 +1,69 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+# ptint(len(thislist))
+
+#index access garna lai
+# print(thislist([1]))
+print(thislist[-1])
+print(thislist[1])
+
+
+#search ::: ctrl + f
+# replace: :: ctrl + h
+
+
+#range access
+# print(thslist[0:2])
+
+#change list item --> aafei herni
+#RANGE MA PANI change
+
+#insrt vAlues
+
+
+
+
+
+
+
+#adding===like append
+#insert --/> bich ma thapna pare parr
+thislist.insert(0, "woho")
+print(thislist)
+# print(len[thislist])
+
+#addh any iteriable
+#extend ley new list's items lai add garxa
+
+
+#remove list
+"""
+remove
+pop
+clear
+delete
+
+
+"""
+
+players= ["ronaldo ", "messi", "naymar", "haland", "mPapaye", "Laifade", "Bipen", "bibek", "Prasun"]
+players.remove("Bipen")
+print(players)
+#index wise hataunii yeta uta aafei padhnii ekchoti
+
+#pop
+players.pop(0)
+print(players)
+
+#delete---> del
+del players[1]
+print(players)
+
+players
+
+#clear --->list ko item hatauna but not list
+#list remains but with no content
+players.clear()
+
+
+#voli loopp
