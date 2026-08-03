88bc365800f39a3a32392431319c548aa14fbc59
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-03 20:05:02 +0545

    Loops and Comprehesion

---
+++ day6.py
@@ -0,0 +1,25 @@
+#loops
+"""
+for loop
+while loop
+
+"""
+
+#you can loop through loop each item
+players = ["bipen", "Bibek", "Pratik", "Ankit", "Sujal"]
+
+for x in players :
+    print(x)
+
+#index use garera garnii bala 
+for x in range(len(players)):
+    print(players[x])
+
+#while loop
+i = 0 
+while i < len(players) :
+    print(players[x])
+    i = i +1
+
+##List Comprehension 
+#offers the Short syntax for looping through list
