9af99be89a1d59a7b1cd207569d8860e9de0dad8
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-03 15:46:43 +0545

    Assi-1 Python Numbers

---
+++ pyNumbers.py
@@ -0,0 +1,61 @@
+#####Python Numbers 
+
+#add two integers
+x= 4
+y= 4
+print(x+y)
+
+
+#Multiply two floting point numbers
+a= 45.7
+b= 23.6
+print(a * b)
+
+#finding reminder
+q = 5
+r = 2
+print(q%r)
+
+#finding Square root
+t = 4
+i = 2
+print(t**i) #ans will be 16
+
+
+""""
+.aayena...Calculate the power of a number.
+"""
+
+#convert float into int
+a = int(a)
+print(type(a))
+
+
+
+"""
+#generate a random Number 
+import random
+
+
+numb = random.randint(1, 100)
+print(numb)
+
+"""
+
+
+##Round a decimal number to two decimal places.
+numbr = 90.449343
+print("before round off:", numbr)
+
+#roundoff
+rounded_numbr = round(numbr, 2)
+print("After Round off: ", rounded_numbr)
+
+
+
+#Calculating simple interest
+
+p = 100
+t = 2
+r = 3000
+print ("I = ", (p*t*r)/ 100) 
