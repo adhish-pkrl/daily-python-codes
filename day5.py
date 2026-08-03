a3c19121fe26972af3449a9d8e6511c66f6db864
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-03 20:06:09 +0545

    Lists

---
+++ day5.py
@@ -0,0 +1,19 @@
+#Lists(day 5 but including introduction it's day 6)
+#multiple item store garna Use hunxa
+#data structure ma parxa
+#build in data types:touple, set, ,,....
+#allows duplicate ,  changable, ordered
+# the_lst = ["apple", "banana", "mango"]
+# print(the_lst)  # it allows duplicate values
+
+# the_lst[1] = "blackcarrot"
+
+thelist = ["apple", "banana", "cherry"]
+print(thelist)
+
+thelist.append("carrot")
+thelist.clear()  #list khali hunxa
+
+#changable
+thelist(len(thelist))
+#{ } --> set ma duplicate value chaldena, aani ordered form ma ni hunna 
