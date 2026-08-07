fad85d3cad33d2999888dd3c2805d1979ae5ed80
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-07 16:59:23 +0545

    Day 8 Set practice

--- d8SetPra.py
+++ d8SetPra.py
@@ -52,3 +52,37 @@ studenta = {"Amrit", "Sujan", "Aayush"}
 studenta.update(names)
 print(studenta)
 print(names)
+
+
+###Intersection
+"""
+the intersection() method will return a new set,
+that only contains the items that are present in both sets
+
+"""
+people = {"Dipesh", "Sagar", "Arjun","Baburam"}
+friends = {"Arjun", "Dipesh","Nerandra", "Bishnu"}
+
+
+#common value print garxa
+bst = people.intersection(friends)
+print("Brothers:",bst)
+
+#different value print garxa
+nono = people.difference(friends)
+print(nono)
+
+#SAME DIFFRENCE nei aauxa
+heii = people - friends
+print(heii)
+
+#diffrence value lai euta set ma add garera Update garxa
+friends.difference_update(people)
+print(friends)
+
+#aabaw difference value lai matrei print garxa --Mathi ko jastei neii same ho
+#EX:
+friends.symmetric_difference(people)
+print(friends)
+
+
