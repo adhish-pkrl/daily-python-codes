5d57e510c7e320375dd2b69b18de7944ff083b4c
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-16 20:51:40 +0545

    Day 13

---
+++ day13.py
@@ -0,0 +1,39 @@
+##Nested Loop
+
+# for x in range(6):
+#     for j in range(4):
+#         pass
+#     print(x)
+
+# for i in range(4):
+#     for k in range(3):
+#         print(i, k)
+
+# print("After this another loop:???")
+# for q in range(4):
+#     for o in range(3):
+#         print(o)
+#     print(q)
+
+
+
+#### range(start, end, encremet/ decrement)
+
+
+print('Anothr loop in Square form:')
+for i in range(1, 7):
+    for j in range(i):
+        print(j, end = " ")
+    print()
+
+
+"""
+
+0 1 2 3 4 5 
+0 1 2 3 4
+0 1 2 3 
+0 1 2 
+0 1
+0
+
+"""
