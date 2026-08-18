de62f1cda7670db4ea57b72e38e1093151263255
adhish-pkrl <adhishpokharel105@gmail.com>
2026-08-18 19:58:05 +0545

    Day-14 Pattern Printing

---
+++ day14.py
@@ -0,0 +1,32 @@
+##pattern printing 
+for i in range(1,7):
+    for j in range(7-i):
+        print(j, end = "")
+    print()
+
+"""
+    *
+   **
+  ***
+ ****
+*****
+"""
+for i in range(1,6):
+    for j in range(5-i):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end = " ")
+    print()
+
+        # for i in range(1,6):
+#     for j in range(5-i):
+#         print(" ",end="" )
+#     for j in range(i):
+#         print("*",)
+ 
+for i in range(1,7):
+    for j in range(7-i):
+        print(" ", end=" ")
+    for k in range(2*i-1):
+        print("*", end = " ")
+    print()
