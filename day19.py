0b44d239879eb8fc259198016ee8cdb7af15c312
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-02 10:29:33 +0545

    Day 19 Decorater, lamda function, Recursion, Array & data and time~!

---
+++ day19.py
@@ -0,0 +1,52 @@
+#DECORATER
+"""
+add a extra behaviour in function without changing the function's code
+
+"""
+
+
+#Lamda Function
+"""
+a lamda function is a small anonymous function
+
+"""
+
+#Example
+# x = lambda vat , total : (vat *100) / total + total
+# print(x(20,400))
+
+
+#Recursion
+"""
+Recursion is when a function calls itself.
+"""
+# def countdown(n):
+#     if n<=0:
+#         print("Done!")
+#     else:
+#         print(n)
+#         countdown(n-1)
+# countdown(5)
+
+
+
+##reange
+#Syntax:
+
+#range(start, stop, step)
+
+
+##Array
+#collection of same data type
+#list of similar data types
+
+
+#modules
+
+
+#data time
+# import calendar
+# calendar.prmonth(2007,07)
+
+import datetime
+datetime.datetime.now("")
