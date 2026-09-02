5697357f08443784e20c82a71a265cb170221a3a
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-02 20:08:44 +0545

    Array

---
+++ testtt.py
@@ -0,0 +1,34 @@
+import numpy
+lst = numpy.array([1,2,3,4])
+print(lst)
+print(type(lst))
+
+
+#as
+import numpy as nmp
+myarr = nmp.array(("Hello", "Sir"))
+print(myarr)
+print(type(myarr))
+
+
+
+#ndarray -- 
+#Dimensional array
+#1D array
+#  [1,2,3,4]--> array vitra
+
+
+#2D array
+#array has 1D array inside an array it is called 2D array 
+
+#3D array
+#with 2D array , both containing two arrays
+
+
+#higher dimentional array
+
+
+
+##aafei array create grni samma hernii
+
+#volii aaru
