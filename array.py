2ef70ee3ef7a3b68715c04d5027ad7159cf37937
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-03 19:22:53 +0545

    Array self practice

---
+++ array.py
@@ -0,0 +1,18 @@
+"""
+an array is a special variable, which can hold 
+    more than one value at a time
+"""
+car1 =" Ford"
+print(car1)
+
+#loop in array
+car11 = "Ford"
+car12 = "BMW"
+car14 = "Toyota"
+cars = ["Mustng", "BMW", "Yemaha", "Thar"]
+print(cars)
+cars.append("TOyota")
+for i in cars:
+    print(i)
+
+
