ae12e6d866435c4e14c24e623427806ed155e1af
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-03 19:22:03 +0545

    DAy 21 Array continue

---
+++ day21.py
@@ -0,0 +1,13 @@
+#Array slicing 
+
+
+#Array data type --> self
+#copy/ view --> self
+
+#Array shape
+import numpy as np
+arr = np.array([[1,2,3,4], [5,6,7,8]])
+print(arr.shape) 
+print(arr.size)
+
+# ndim
