d75678c35961c81f4c0ad0c4d0a23062e11e962b
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-04 20:20:54 +0545

    Modules

---
+++ modules.py
@@ -0,0 +1,17 @@
+##Modules
+"""
+consider a module to be the same as code library
+
+"""
+def greeting(name):
+    print("Namsate🙏" +name)
+
+print("this module is accessed in NumPy.py file")
+
+
+
+person1 = {
+    "name": "John",
+    "age" : 19,
+    "country" : "Nepal"
+}
