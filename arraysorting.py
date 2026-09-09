b31ca4cdcd5cbb176f0991ba24475a66e9769842
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-09 12:02:02 +0545

    Array SOrting only

---
+++ arraysorting.py
@@ -0,0 +1,28 @@
+### NumPy Sorting Arrays
+"""
+sorting means putting element in an ordered sequence.
+
+    Ordered Sequesnce is any sequence that has an order..
+    corresopnding to element, like numeric or alphabetical,
+     ascending or decending.
+"""
+
+import numpy as np
+
+arr= np.array([3,2,0,1])
+print(np.sort(arr))
+
+
+
+####Sort alphaBetical order
+ary = np.array(["banana", "quiwe", "apple", "orange"])
+print(np.sort(ary))
+
+
+
+###Sorting 2-D array
+sarr = np.array([[3,6,1], [3,8,2]])
+print(np.sort(sarr))
+
+
+
