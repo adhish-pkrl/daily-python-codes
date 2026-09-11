2621b8bce0f966052f102e883216a8a3a1087173
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-11 20:21:13 +0545

    concept of pandas

---
+++ first_pand.py
@@ -0,0 +1,17 @@
+##Pandas
+"""
+working with data set-data set- excel...
+    *analyizing
+    *cleaning
+    *exploring
+    *manipulation
+
+    name has a reference to both "panel data",,
+    and "python data anlysis"
+"""
+
+#Why it used
+"""
+Analyse big data
+Stastics data 
+"""
