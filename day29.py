64845c7cfb8059bf428454157323f3a965937b2d
adhish-pkrl <adhishpokharel105@gmail.com>
2026-09-20 21:24:41 +0545

    Day 29 -> Columns name Change

---
+++ day29.py
@@ -0,0 +1,23 @@
+#Data cleaning
+
+
+#Columns name--Change->
+import pandas as pd
+df = pd.read_csv('unclean_data.csv')
+print(df)
+
+"""
+bad data set =  NaN, 
+                duplicate names,
+                diff data types
+
+"""
+#Empty cell
+"""
+Since data sets can be very big, and removing a few 
+    rows will not have a big impact on the result
+"""
+# syntah  = dropna() 
+#change name
+
+#Delete Cell
